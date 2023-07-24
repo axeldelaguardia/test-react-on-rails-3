875e750d1a64b4fb07bb35e8e17dc4509bd31e3d
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-07-24 15:23:26 -0600

    add delete action to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: session_params[:email].downcase)
-		require 'pry'; binding.pry
 		if user&.authenticate(params[:session][:password])
 			log_in(user)
 			redirect_to "/dashboard"
@@ -14,6 +13,11 @@ class SessionsController < ApplicationController
 			flash.now[:danger] = "Invalid email/password combination"
 			render "new"
 		end
+	end
+
+	def destroy
+		log_out
+		redirect_to login_path
 	end
 
 	private
