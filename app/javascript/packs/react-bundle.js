b08ad5879a33afbbd2a7b9e985c4cb7d6788e13d
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-07-26 10:26:40 -0600

    register new components

--- app/javascript/packs/react-bundle.js
+++ app/javascript/packs/react-bundle.js
@@ -1,18 +1,24 @@
 import ReactOnRails from 'react-on-rails';
 
-import HomePage from '../bundles/Dashboard/components/HomePage';
+import PageLayout from '../bundles/Dashboard/components/PageLayout';
 import LoginForm from '../bundles/Dashboard/components/LoginForm';
+import Settings from '../bundles/Dashboard/components/Settings';
+import AccountInfo from '../bundles/Dashboard/components/SettingsComponents/AccountInfo';
+import UpdateUserInfoModal from '../bundles/Dashboard/components/SettingsComponents/UpdateUserInfo';
+import TimeZoneDropdown from '../bundles/Dashboard/components/SettingsComponents/TimeZoneDropdown';
+import NavBar from '../bundles/Dashboard/components/NavBar';
 import Dashboard from '../bundles/Dashboard/components/Dashboard';
-import AccountInfo from '../bundles/Dashboard/components/AccountInfo';
-import UpdateUserInfoModal from '../bundles/Dashboard/components/UpdateUserInfo';
-import TimeZoneDropdown from '../bundles/Dashboard/components/TimeZoneDropdown';
+import UpdateAccount from '../bundles/Dashboard/components/SettingsComponents/UpdateAccount';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HomePage,
+  PageLayout,
 	LoginForm,
-	Dashboard,
+	Settings,
 	AccountInfo,
 	UpdateUserInfoModal,
 	TimeZoneDropdown,
+	NavBar,
+	Dashboard,
+	UpdateAccount,
 });
