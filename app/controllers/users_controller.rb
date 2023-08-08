c5d3162292d85206a19eddf2d5b28fc35333baff
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-08-07 21:40:14 -0600

    add aws s3 bucket service to save & retrieve user images from s3 bucket

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,57 +2,29 @@ class UsersController < ApplicationController
 	layout "react"
 
 	def show
-		if current_user
-			user = current_user
-			@user_props = { 
-				user: user, 
-				name: user.name, 
-				email: user.email, 
-				timezone: user.timezone, 
-				image_path: user.image_path,
-				background_path: user.background_path
-			}
-		else
-			flash[:danger] = "You must be logged in to view this page."
-			redirect_to root_path
-		end
+		set_user_props(current_user)
 	end
 
 	def edit
-		if current_user
-			user = current_user
-			@user_props = { 
-				user: user, 
-				name: user.name, 
-				email: user.email, 
-				timezone: user.timezone, 
-				image_path: user.image_path,
-				background_path: user.background_path
-			}
-		else
-			flash[:danger] = "You must be logged in to view this page."
-			redirect_to root_path
-		end
+		set_user_props(current_user)
 	end
 
 	def update
 		user = current_user
 		if (!user_params[:background_path]) && authenticate(user)
 			params[:user][:new_password] ? update_password(user) : user.update(user_params)
-			render json: { user: user }
 		elsif user_params[:background_path]
 			user.update(user_params)
-			render json: { user: user }
 		else
 			flash[:danger] = "Incorrect password."
 		end
+		render json: { user: user }
 	end
 
 	def upload_image
 		user = current_user
 		if params[:file]
-			save_profile_pic(params[:file])
-			user.update(image_path: "/uploads/#{user.id}/#{params[:file].original_filename}")
+			S3Service.new.upload_image(user.id, params[:file])
 		end
 		redirect_to settings_path
 	end
@@ -60,7 +32,13 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :timezone, :background_path)
+		params.require(:user).permit(
+			:name, 
+			:email, 
+			:timezone, 
+			:background_path, 
+			:photo
+		)
 	end
 
 	def authenticate(user)
@@ -73,17 +51,22 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def save_profile_pic(file)
-		user_img_dir_path = Rails.root.join('public', 'uploads', current_user.id.to_s)
-		Dir.mkdir(user_img_dir_path) unless Dir.exist?(user_img_dir_path)
-	
-		begin
-			File.open(user_img_dir_path.join(file.original_filename), 'wb') do |f|
-				f.write(file.read)
-			end
-		rescue IOError => e
-			puts "IOError occurred: #{e.message}"
+	def require_login
+		unless current_user
+			flash[:danger] = "You must be logged in to view this page."
+			redirect_to root_path
 		end
 	end
-	
+
+	def set_user_props(user)
+		profile_picture = S3Service.new.get_image(user.id)
+		@user_props = {
+			user: user,
+			name: user.name,
+			email: user.email,
+			timezone: user.timezone,
+			image_path: profile_picture,
+			background_path: user.background_path
+		}
+	end
 end
---
+++ app/services/s3_service.rb
@@ -0,0 +1,22 @@
+class S3Service
+	def upload_image(user_id, image)
+		image = File.open(image)
+		response = connection.put(user_id.to_s) do |req|
+			req['Content-Type'] = 'image/jpeg'
+			req['Content-Length'] = image.size.to_s
+			req.body = Faraday::Multipart::FilePart.new(image, 'image/jpeg')
+		end
+	end
+
+	def get_image(user_id)
+		response = connection.get(user_id.to_s)
+		Base64.strict_encode64(response.body)
+	end
+
+	private
+	def connection
+		Faraday.new(url: 'https://nakopyv5pg.execute-api.us-east-2.amazonaws.com/v1/wled-bucket/') do |f|
+			f.request :multipart
+		end
+	end
+end
