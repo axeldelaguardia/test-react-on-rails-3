6d10b4542f3d515ff350a76fdce39e2eda405d59
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-08-09 12:17:46 -0600

    add wled devices and effects to user's dashboard

--- app/javascript/bundles/Dashboard/components/Dashboard.jsx
+++ app/javascript/bundles/Dashboard/components/Dashboard.jsx
@@ -1,47 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import PageLayout from './PageLayout'
 import style from './Dashboard.module.scss'
+import axios from 'axios';
 
-const Dashboard = ({user, background_path, name, email}) => {
-	const handleOnMouseMoveEvent = (e) => {
-		const { currentTarget: target } = e;
+const Dashboard = ({ user, background_path, name, wleds }) => {
+	const [deviceInfoList, setDeviceInfoList] = useState([]);
 
-		const rect = target.getBoundingClientRect(),
-			x = e.clientX - rect.left,
-			y = e.clientY - rect.top;
+	const fetchDeviceInfo = async (wled) => {
+		const url = `http://${wled}/json`;
+		try {
+			const response = await axios.get(url);
+			const deviceInfo = response.data;
+			console.log(deviceInfo);
+			return deviceInfo;
+		} catch (error) {
+			console.log(error);
+			return null;
+		}
+	};
 
-		target.style.setProperty('--mouse-x', `${x}px`);
-		target.style.setProperty('--mouse-y', `${y}px`);
+	useEffect(() => {
+		const fetchDeviceInformation = async () => {
+			const updateDeviceInfoList = await Promise.all(wleds.map(async (wled) => {
+				const deviceInfo = await fetchDeviceInfo(wled);
+				return { state: deviceInfo.state, info: deviceInfo.info }
+			}));
+			setDeviceInfoList(updateDeviceInfoList);
+		};
+		fetchDeviceInformation();
+	}, []);
+
+	const handleCardClick = async (wled) => {
+		console.log('card clicked for ' + wled);
+		const url = `http://${wled}/json/state`;
+		console.log(url);
+		const data = { on: 't', v: true };
+		try {
+			const state = await axios.post(url, data);
+			console.log(state);
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
-	// for(const card of document.querySelectorAll(".card")) {
-	// 	card.onmousemove = e => handleOnMouseMoveEvent(e);
-	// 	console.log(card)
-	// }
+	const handleButtonClick = (e) => {
+		e.stopPropagation();
+		console.log(deviceInfoList);
+	};
 
 	return (
 		<PageLayout user={user} background={background_path} className={style.mainLayout}>
 			<div className={style.cardBody}>
 				<div className={style.cards}>
-					<div className={style.card}>
-						<div className={style.cardContent}>
-							{/* <div class="card-image">
-								<i class="fa-duotone fa-apartment"></i>
+					{deviceInfoList.map((wled, index) => (
+						<div 
+							className={style.card} 
+							onClick={() => handleCardClick(wled.info.ip)}
+							key={index}
+						>
+							<div className={style.cardContent}>
+								<img className={style.cardImage} src={'images/001_cheerful.png'} />
+								<h3>{wled.info.name}</h3>
+								<button className={style.cardMenu} onClick={handleButtonClick}>settings</button>
 							</div>
-							<div class="card-info-wrapper">
-								<div class="card-info">
-									<i class="fa-duotone fa-apartment"></i>
-									<div class="card-info-title">
-										<h3>Apartments</h3>  
-										<h4>Places to be apart. Wait, what?</h4>
-									</div>    
-								</div>
-							</div> */}
 						</div>
-					</div>
-					<div className={style.card}>
-						{/* <div class="card-content">
+					))};
+					{/* <div className={style.card}>
+						<div class="card-content">
 							<div class="card-image">
 								<i class="fa-duotone fa-unicorn"></i>
 							</div>
@@ -54,10 +80,10 @@ const Dashboard = ({user, background_path, name, email}) => {
 									</div>    
 								</div>  
 							</div>
-						</div> */}
+						</div>
 					</div>
 					<div className={style.card}>
-						{/* <div class="card-content">
+						<div class="card-content">
 							<div class="card-image">
 								<i class="fa-duotone fa-blender-phone"></i>
 							</div>
@@ -70,10 +96,10 @@ const Dashboard = ({user, background_path, name, email}) => {
 									</div>    
 								</div>
 							</div>
-						</div> */}
+						</div>
 					</div>
 					<div className={style.card}>
-						{/* <div class="card-content">
+						<div class="card-content">
 							<div class="card-image">
 								<i class="fa-duotone fa-person-to-portal"></i>
 							</div>
@@ -86,10 +112,10 @@ const Dashboard = ({user, background_path, name, email}) => {
 									</div>    
 								</div>
 							</div>
-						</div> */}
+						</div>
 					</div>
 					<div className={style.card}>
-						{/* <div class="card-content">
+						<div class="card-content">
 							<div class="card-image">
 								<i class="fa-duotone fa-person-from-portal"></i>
 							</div>
@@ -102,10 +128,10 @@ const Dashboard = ({user, background_path, name, email}) => {
 									</div>    
 								</div>
 							</div>
-						</div> */}
+						</div>
 					</div>
 					<div className={style.card}>
-						{/* <div class="card-content">
+						<div class="card-content">
 							<div class="card-image">
 								<i class="fa-duotone fa-otter"></i>
 							</div>
@@ -118,8 +144,8 @@ const Dashboard = ({user, background_path, name, email}) => {
 									</div>    
 								</div>
 							</div>
-						</div> */}
-					</div>
+						</div>
+					</div> */}
 				</div>
 			</div>
 		</PageLayout>
@@ -129,8 +155,8 @@ const Dashboard = ({user, background_path, name, email}) => {
 Dashboard.propTypes = {
 	user: PropTypes.object,
 	name: PropTypes.string,
-	email: PropTypes.string,
 	background_path: PropTypes.string,
+	wleds: PropTypes.array,
 };
 
 export default Dashboard;
